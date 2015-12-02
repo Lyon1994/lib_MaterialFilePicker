1470a981c73093c1c80cb62ee4fbb7403f3a972c
Lyon_Yan <765211630@qq.com>
2015-12-02 12:34:24 +0800

    update

--- src/com/nbsp/materialfilepicker/ui/FilePickerActivity.java
+++ src/com/nbsp/materialfilepicker/ui/FilePickerActivity.java
@@ -20,129 +20,152 @@ import java.lang.reflect.Field;
 /**
  * Created by Dimorinny on 24.10.15.
  */
-public class FilePickerActivity extends AppCompatActivity implements DirectoryFragment.FileClickListener {
-    private static final String ARG_CURRENT_PATH = "arg_title_state";
-    public static final String RESULT_FILE_PATH = "result_file_path";
-    private static final String START_PATH = Environment.getExternalStorageDirectory().getAbsolutePath();
-    private static final int HANDLE_CLICK_DELAY = 150;
+public class FilePickerActivity extends AppCompatActivity implements
+		DirectoryFragment.FileClickListener {
+	private static final String ARG_CURRENT_PATH = "arg_title_state";
+	public static final String RESULT_FILE_PATH = "result_file_path";
+	private static String START_PATH = Environment
+			.getExternalStorageDirectory().getAbsolutePath();
+	private static final int HANDLE_CLICK_DELAY = 150;
 
-    private Toolbar mToolbar;
-    private String mCurrentPath = START_PATH;
+	/**
+	 * 
+	 * @author Lyon_Yan <br/>
+	 *         <b>time</b>: 2015年12月2日 下午12:32:40
+	 * @param sTART_PATH
+	 */
+	public static void setSTART_PATH(String sTART_PATH) {
+		START_PATH = sTART_PATH;
+	}
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_file_picker);
+	/**
+	 * 
+	 * @author Lyon_Yan <br/>
+	 *         <b>time</b>: 2015年12月2日 下午12:32:44
+	 * @return
+	 */
+	public static String getSTART_PATH() {
+		return START_PATH;
+	}
 
-        if (savedInstanceState != null) {
-            mCurrentPath = savedInstanceState.getString(ARG_CURRENT_PATH);
-        } else {
-            initFragment();
-        }
+	private Toolbar mToolbar;
+	private String mCurrentPath = START_PATH;
 
-        initViews();
-        initToolbar();
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_file_picker);
 
-    private void initToolbar() {
-        setSupportActionBar(mToolbar);
+		if (savedInstanceState != null) {
+			mCurrentPath = savedInstanceState.getString(ARG_CURRENT_PATH);
+		} else {
+			initFragment();
+		}
 
-        // Show back button
-        if (getSupportActionBar() != null) {
-            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        }
+		initViews();
+		initToolbar();
+	}
 
-        // Truncate start of toolbar title
-        try {
-            Field f = mToolbar.getClass().getDeclaredField("mTitleTextView");
-            f.setAccessible(true);
+	private void initToolbar() {
+		setSupportActionBar(mToolbar);
 
-            TextView textView = (TextView) f.get(mToolbar);
-            textView.setEllipsize(TextUtils.TruncateAt.START);
-        } catch (Exception ignored) {}
+		// Show back button
+		if (getSupportActionBar() != null) {
+			getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+		}
 
-        updateTitle();
-    }
+		// Truncate start of toolbar title
+		try {
+			Field f = mToolbar.getClass().getDeclaredField("mTitleTextView");
+			f.setAccessible(true);
 
-    private void initViews() {
-        mToolbar = (Toolbar) findViewById(R.id.toolbar);
-    }
+			TextView textView = (TextView) f.get(mToolbar);
+			textView.setEllipsize(TextUtils.TruncateAt.START);
+		} catch (Exception ignored) {
+		}
 
-    private void initFragment() {
-        getFragmentManager().beginTransaction()
-                .add(R.id.container, DirectoryFragment.getInstance(START_PATH))
-                .commit();
-    }
+		updateTitle();
+	}
 
-    private void updateTitle() {
-        if (getSupportActionBar() != null) {
-            String title = mCurrentPath.isEmpty() ? "/" : mCurrentPath;
-            if (title.startsWith(START_PATH)) {
-                title = title.replaceFirst(START_PATH, getString(R.string.start_path_name));
-            }
-            getSupportActionBar().setTitle(title);
-        }
-    }
+	private void initViews() {
+		mToolbar = (Toolbar) findViewById(R.id.toolbar);
+	}
 
-    private void addFragmentToBackStack(String path) {
-        getFragmentManager().beginTransaction()
-                .replace(R.id.container, DirectoryFragment.getInstance(path))
-                .addToBackStack(null)
-                .commit();
-    }
+	private void initFragment() {
+		getFragmentManager().beginTransaction()
+				.add(R.id.container, DirectoryFragment.getInstance(START_PATH))
+				.commit();
+	}
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem menuItem) {
-        if (menuItem.getItemId() == android.R.id.home) {
-            onBackPressed();
-        }
-        return super.onOptionsItemSelected(menuItem);
-    }
+	private void updateTitle() {
+		if (getSupportActionBar() != null) {
+			String title = mCurrentPath.isEmpty() ? "/" : mCurrentPath;
+			if (title.startsWith(START_PATH)) {
+				title = title.replaceFirst(START_PATH,
+						getString(R.string.start_path_name));
+			}
+			getSupportActionBar().setTitle(title);
+		}
+	}
 
-    @Override
-    public void onBackPressed(){
-        FragmentManager fm = getFragmentManager();
+	private void addFragmentToBackStack(String path) {
+		getFragmentManager().beginTransaction()
+				.replace(R.id.container, DirectoryFragment.getInstance(path))
+				.addToBackStack(null).commit();
+	}
 
-        if (fm.getBackStackEntryCount() > 0) {
-            fm.popBackStack();
-            mCurrentPath = FileUtils.cutLastSegmentOfPath(mCurrentPath);
-            updateTitle();
-        } else {
-            setResult(RESULT_CANCELED);
-            super.onBackPressed();
-        }
-    }
+	@Override
+	public boolean onOptionsItemSelected(MenuItem menuItem) {
+		if (menuItem.getItemId() == android.R.id.home) {
+			onBackPressed();
+		}
+		return super.onOptionsItemSelected(menuItem);
+	}
 
-    @Override
-    public void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putString(ARG_CURRENT_PATH, mCurrentPath);
-    }
+	@Override
+	public void onBackPressed() {
+		FragmentManager fm = getFragmentManager();
 
-    @Override
-    public void onFileClicked(final File clickedFile) {
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                handleFileClicked(clickedFile);
-            }
-        }, HANDLE_CLICK_DELAY);
-    }
+		if (fm.getBackStackEntryCount() > 0) {
+			fm.popBackStack();
+			mCurrentPath = FileUtils.cutLastSegmentOfPath(mCurrentPath);
+			updateTitle();
+		} else {
+			setResult(RESULT_CANCELED);
+			super.onBackPressed();
+		}
+	}
 
-    private void handleFileClicked(final File clickedFile) {
-        if (clickedFile.isDirectory()) {
-            addFragmentToBackStack(clickedFile.getPath());
-            mCurrentPath = clickedFile.getPath();
-            updateTitle();
-        } else {
-            setResultAndFinish(clickedFile.getPath());
-        }
-    }
+	@Override
+	public void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		outState.putString(ARG_CURRENT_PATH, mCurrentPath);
+	}
 
-    private void setResultAndFinish(String filePath) {
-        Intent data = new Intent();
-        data.putExtra(RESULT_FILE_PATH, filePath);
-        setResult(RESULT_OK, data);
-        finish();
-    }
+	@Override
+	public void onFileClicked(final File clickedFile) {
+		new Handler().postDelayed(new Runnable() {
+			@Override
+			public void run() {
+				handleFileClicked(clickedFile);
+			}
+		}, HANDLE_CLICK_DELAY);
+	}
+
+	private void handleFileClicked(final File clickedFile) {
+		if (clickedFile.isDirectory()) {
+			addFragmentToBackStack(clickedFile.getPath());
+			mCurrentPath = clickedFile.getPath();
+			updateTitle();
+		} else {
+			setResultAndFinish(clickedFile.getPath());
+		}
+	}
+
+	private void setResultAndFinish(String filePath) {
+		Intent data = new Intent();
+		data.putExtra(RESULT_FILE_PATH, filePath);
+		setResult(RESULT_OK, data);
+		finish();
+	}
 }
